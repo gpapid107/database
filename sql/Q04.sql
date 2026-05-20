7b7f46573ccdb59715332d15b43f3842701bc0bd
gpapid107 <maltose.basket-0i@icloud.com>
2026-05-20 18:11:00 +0300

    Update Q04.sql

--- sql/Q04.sql
+++ sql/Q04.sql
@@ -4,5 +4,5 @@ SELECT
     AVG(Evaluation.Overall_Experience) AS avg_overall_hospitalization_experience
 FROM Doctor_Evaluation
 JOIN Evaluation ON Doctor_Evaluation.AdmissionID = Evaluation.AdmissionID
-WHERE Doctor_Evaluation.Doctor_AMKA = '98182992299'
+WHERE Doctor_Evaluation.Doctor_AMKA = '10000000324'
 GROUP BY Doctor_Evaluation.Doctor_AMKA;
