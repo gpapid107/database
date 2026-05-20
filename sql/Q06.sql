e266b1a3a7d625935f9353cffc868c8afa51b784
gpapid107 <maltose.basket-0i@icloud.com>
2026-05-20 18:06:47 +0300

    Update Q06.sql

--- sql/Q06.sql
+++ sql/Q06.sql
@@ -2,4 +2,4 @@ SELECT A.Admission_Date, A.Release_Date, A.Total_Cost, A.Department_Name, A.Admi
 A.Release_Diagnosis_ICD_10_Code, (E.Nursing_Quality+E.Cleanliness+E.Food+E.Overall_Experience)/4.0 AS Average_Evaluation 
 FROM Admission A 
 LEFT OUTER JOIN Evaluation E ON A.AdmissionID = E.AdmissionID 
-WHERE A.Patient_AMKA = '55799802841' ORDER BY A.Admission_Date;
+WHERE A.Patient_AMKA = '40000000004' ORDER BY A.Admission_Date;
